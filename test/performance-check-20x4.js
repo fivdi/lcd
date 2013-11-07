8e0a42f707fd4d1697bf3a92ab5ed4b39de0205e
fivdi <fivdiweb@gmail.com>
2013-11-07 15:38:00 +0100

    get performance check to print results

--- test/performance-check-20x4.js
+++ test/performance-check-20x4.js
@@ -1,15 +1,30 @@
+/*
+ * Fill the display with each character and determine how often the display
+ * can be filled per second.
+ */
 var Lcd = require('../lcd'),
-  lcd = new Lcd({rs:45, e:44, data:[66, 67, 68, 69], cols:20, rows:4});
+  lcd = new Lcd({rs:45, e:44, data:[66, 67, 68, 69], cols:20, rows:4}),
+  time;
 
 lcd.on('ready', function () {
+  time = process.hrtime();
   fillDisplay(0);
 });
 
 function fillDisplay(charCode) {
   lcd.print(Array(20 * 4 + 1).join(String.fromCharCode(charCode)));
 
-  setTimeout(function () {
-    print((charCode + 1) % 256)
-  }, 250);
+  process.nextTick(function () {
+    var fillsPerSec;
+
+    charCode += 1;
+    if (charCode < 256) {
+      fillDisplay(charCode)
+    } else {
+      time = process.hrtime(time);
+      fillsPerSec  = Math.floor(256 / (time[0] + time[1] / 1E9));
+      console.log(fillsPerSec + ' display fills per second');
+    }
+  });
 }
 
