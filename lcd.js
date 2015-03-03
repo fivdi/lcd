dc7fc7e3fd0dcdefffbf50104425d6fd7927c2aa
Kassandra Perch <kassandra@kassandraperch.com>
2015-03-02 21:02:33 -0600

    Got rid of the magic numbers and replaced them with a command map

--- lcd.js
+++ lcd.js
@@ -6,9 +6,25 @@ var EventEmitter = require('events').EventEmitter,
   util = require('util'),
   tick = global.setImmediate || process.nextTick;
 
-var ROW_OFFSETS = [0x00, 0x40, 0x14, 0x54];
+var __ROW_OFFSETS = [0x00, 0x40, 0x14, 0x54];
 
-// TODO - Don't use magic numbers, define constants instead.
+var __COMMANDS = {
+  CLEAR_DISPLAY: 0x01,
+  HOME: 0x02,
+  SET_CURSOR: 0x80,
+  DISPLAY_ON: 0x04,
+  DISPLAY_OFF: ~0x04,
+  CURSOR_ON: 0x02,
+  CURSOR_OFF: ~0x02,
+  BLINK_ON: 0x01,
+  BLINK_OFF: ~0x01,
+  SCROLL_LEFT: 0x18,
+  SCROLL_RIGHT: 0x1c,
+  LEFT_TO_RIGHT: 0x02,
+  RIGHT_TO_LEFT: ~0x02,
+  AUTOSCROLL_ON: 0x01,
+  AUTOSCROLL_OFF: ~0x01
+}
 
 function Lcd(config) {
   var i;
@@ -113,74 +129,75 @@ Lcd.prototype._printChar = function (str, index, cb) {
 
 Lcd.prototype.clear = function (cb) {
   // Wait > 1.52ms. There were issues waiting for 2ms so wait 3ms.
-  this._commandAndDelay(0x01, 3, 'clear', cb);
+  this._commandAndDelay(__COMMMANDS.CLEAR_DISPLAY, 3, 'clear', cb);
 };
 
 Lcd.prototype.home = function (cb) {
   // Wait > 1.52ms. There were issues waiting for 2ms so wait 3ms.
-  this._commandAndDelay(0x02, 3, 'home', cb);
+  this._commandAndDelay(__COMMANDS.HOME, 3, 'home', cb);
 };
 
 Lcd.prototype.setCursor = function (col, row) {
-  var r = row > this.rows ? this.rows - 1 : row;
-  this._command(0x80 | (col + ROW_OFFSETS[r]));
+  var r = row > this.rows ? this.rows - 1 : row; //TODO: throw error instead? Seems like this could cause a silent bug.
+  //we don't check for column because scrolling is a possibility. Should we check if it's in range if scrolling is off?
+  this._command(__COMMANDS.SET_CURSOR | (col + __ROW_OFFSETS[r]));
 };
 
 Lcd.prototype.display = function () {
-  this.displayControl |= 0x04;
+  this.displayControl |= __COMMANDS.DISPLAY_ON;
   this._command(this.displayControl);
 };
 
 Lcd.prototype.noDisplay = function () {
-  this.displayControl &= ~0x04;
+  this.displayControl &= __COMMANDS.DISPLAY_OFF;
   this._command(this.displayControl);
 };
 
 Lcd.prototype.cursor = function () {
-  this.displayControl |= 0x02;
+  this.displayControl |= __COMMANDS.CURSOR_ON;
   this._command(this.displayControl);
 };
 
 Lcd.prototype.noCursor = function () {
-  this.displayControl &= ~0x02;
+  this.displayControl &= __COMMANDS.CURSOR_OFF;
   this._command(this.displayControl);
 };
 
 Lcd.prototype.blink = function () {
-  this.displayControl |= 0x01;
+  this.displayControl |= __COMMANDS.BLINK_ON;
   this._command(this.displayControl);
 };
 
 Lcd.prototype.noBlink = function () {
-  this.displayControl &= ~0x01;
+  this.displayControl &= __COMMANDS.BLINK_OFF;
   this._command(this.displayControl);
 };
 
 Lcd.prototype.scrollDisplayLeft = function () {
-  this._command(0x18);
+  this._command(__COMMANDS.SCROLL_LEFT);
 };
 
 Lcd.prototype.scrollDisplayRight = function () {
-  this._command(0x1c);
+  this._command(__COMMANDS.SCROLL_RIGHT);
 };
 
 Lcd.prototype.leftToRight = function () {
-  this.displayMode |= 0x02;
+  this.displayMode |= __COMMANDS.LEFT_TO_RIGHT;
   this._command(this.displayMode);
 };
 
 Lcd.prototype.rightToLeft = function () {
-  this.displayMode &= ~0x02;
+  this.displayMode &= __COMMANDS.RIGHT_TO_LEFT;
   this._command(this.displayMode);
 };
 
 Lcd.prototype.autoscroll = function () {
-  this.displayMode |= 0x01;
+  this.displayMode |= __COMMANDS.AUTOSCROLL_ON;
   this._command(this.displayMode);
 };
 
 Lcd.prototype.noAutoscroll = function () {
-  this.displayMode &= ~0x01;
+  this.displayMode &= __COMMANDS.AUTOSCROLL_OFF;
   this._command(this.displayMode);
 };
 
